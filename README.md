2169bd12156bb7697eeec98485bd47ff21b0fa28
yuriagopian <yuri.agopian@hotmail.com>
2023-04-20 13:53:17 -0300

    chore: create chat db repository

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ $ migrate create -ext=mysql -dir=sql/migrations -seq init
 ## Generate sqlc
 
 sqlc generate
+
+## References
+
+https://github.com/golang-migrate/migrate/tree/master/cmd/migrate
+
+## Issues
+
+https://github.com/rubenv/sql-migrate/issues/213
--- internal/infra/repository/chat.go
+++ internal/infra/repository/chat.go
@@ -1,8 +1,11 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
+	"time"
 
+	"github.com/yuriagopian/golang-chat-service/internal/domain/entity"
 	"github.com/yuriagopian/golang-chat-service/internal/infra/db"
 )
 
@@ -16,4 +19,34 @@ func NewChatRepositoryMySQL(dbt *sql.DB) *ChatRepositoryMySQL {
 		DB:      dbt,
 		Queries: db.New(dbt),
 	}
+}
+
+func (r *ChatRepositoryMySQL) CreateChat(ctx context.Context, chat *entity.Chat) error {
+	err := r.Queries.CreateChat(
+		ctx,
+		db.CreateChatParams{
+			ID:               chat.ID,
+			UserID:           chat.UserID,
+			InitialMessageID: chat.InitialSystemMessage.Content,
+			Status:           chat.Status,
+			TokenUsage:       int32(chat.TokenUsage),
+			Model:            chat.Config.Model.Name,
+			ModelMaxTokens:   int32(chat.Config.MaxTokens),
+			Temperature:      float64(chat.Config.Temperature),
+			TopP:             float64(chat.Config.TopP),
+			N:                int32(chat.Config.N),
+			Stop:             chat.Config.Stop[0],
+			MaxTokens:        int32(chat.Config.MaxTokens),
+			PresencePenalty:  float64(chat.Config.PresencePenalty),
+			FrequencyPenalty: float64(chat.Config.FrequencyPenalty),
+			CreatedAt:        time.Now(),
+			UpdatedAt:        time.Now(),
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }

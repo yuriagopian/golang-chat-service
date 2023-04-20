9a9fcc2c635c0f02aca6e8a12fbca3182c71ae8d
yuriagopian <yuri.agopian@hotmail.com>
2023-04-19 23:43:05 -0300

    feat: chat

--- internal/domain/entity/chat.go
+++ internal/domain/entity/chat.go
@@ -1,1 +1,12 @@
 package entity
+
+type ChatConfig struct {
+	Model *Model
+}
+type Chat struct {
+	ID         string
+	UserID     string
+	Status     string
+	TokenUsage int
+	Config     *ChatConfig
+}

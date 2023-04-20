aefb55a0e8ed163cad5338bc074c93fbece3436e
yuriagopian <yuri.agopian@hotmail.com>
2023-04-20 09:47:43 -0300

    feat: chat completion use case dto

--- internal/usecases/chatcompletionstream/completion.go
+++ internal/usecases/chatcompletionstream/completion.go
@@ -5,6 +5,32 @@ import (
 	"github.com/yuriagopian/golang-chat-service/internal/domain/gateway"
 )
 
+type ChatCompletionConfigInputDTO struct {
+	Model                string
+	ModelMaxTokens       int
+	Temperature          float32
+	TopP                 float32
+	N                    int
+	Stop                 []string
+	MaxTokens            int
+	PresencePenalty      float32
+	FrequencyPenalty     float32
+	InitialSystemMessage string
+}
+
+type ChatCompletionInputDTO struct {
+	ChatID      string
+	UserID      string
+	UserMessage string
+	Config      ChatCompletionConfigInputDTO
+}
+
+type ChatCompletionOutputDTO struct {
+	ChatID  string
+	UserID  string
+	Content string
+}
+
 type ChatCompletionUseCase struct {
 	ChatGateway  gateway.ChatGateway
 	OpenAiClient *openai.Client
